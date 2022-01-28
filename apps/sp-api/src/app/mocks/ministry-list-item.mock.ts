9b0d337464423c94a26c9190e21730a22bf1ae66
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-01-28 16:06:39 -0300

    🏗 build(lib/interfaces): change ministry list item interface name

--- apps/sp-api/src/app/mocks/ministry-list-item.mock.ts
+++ apps/sp-api/src/app/mocks/ministry-list-item.mock.ts
@@ -1,6 +1,6 @@
-import { MinistryListItem } from '@sp/shared-interfaces';
+import { MinistryListItemResponse } from '@sp/shared-interfaces';
 
-export const MINISTRY_LIST_ITEM_MOCK: MinistryListItem[] = [
+export const MINISTRY_LIST_ITEM_MOCK: MinistryListItemResponse[] = [
   {
     ministryID: 1,
     name: 'Sara Nossa Terra',
