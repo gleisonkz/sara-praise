a95e1bbe3b0ae42616a980bcdf5f43dc078dbe4c
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-01-28 16:00:21 -0300

    📦 refactor(api): add mocks

---
+++ apps/sp-api/src/app/mocks/ministry-list-item.mock.ts
@@ -0,0 +1,12 @@
+import { MinistryListItem } from '@sp/shared-interfaces';
+
+export const MINISTRY_LIST_ITEM_MOCK: MinistryListItem[] = [
+  {
+    ministryID: 1,
+    name: 'Sara Nossa Terra',
+  },
+  {
+    ministryID: 2,
+    name: 'Lagoinha',
+  },
+];
