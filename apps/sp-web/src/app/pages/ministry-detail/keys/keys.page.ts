608dcd2b854b9f10a1563952a36eab9e6e3d9010
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-01-29 10:15:11 -0300

    ✨ feat(web): keys page

---
+++ apps/sp-web/src/app/pages/ministry-detail/keys/keys.page.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  templateUrl: './keys.page.html',
+  styleUrls: ['./keys.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class KeysPage {}
