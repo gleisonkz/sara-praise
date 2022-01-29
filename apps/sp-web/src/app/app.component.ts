02f9ba6a5e3dd192b542a1cf9ce5499e7eab7ac6
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-01-29 08:49:22 -0300

    💄 style(web): remove unused code

--- apps/sp-web/src/app/app.component.ts
+++ apps/sp-web/src/app/app.component.ts
@@ -1,11 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'sp-root',
   template: ` <router-outlet></router-outlet> `,
 })
-export class AppComponent {
-  hello$ = this.http.get('/api/ministry');
-  constructor(private http: HttpClient) {}
-}
+export class AppComponent {}
