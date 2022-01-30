f661253aa6d0d9fe24f98bd53a454c29e99aff95
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-01-30 00:40:45 -0300

    ✨ feat(web): auth interceptor

---
+++ apps/sp-web/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+    HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { TokenService } from '@sp/web/services';
+
+import { Observable } from 'rxjs';
+
+const TOKEN_HEADER_KEY = 'Authorization'; // for Spring Boot back-end
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private tokenService: TokenService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let authReq = req;
+    const token = this.tokenService.getToken();
+
+    if (!token) return next.handle(authReq);
+
+    const authToken = `Bearer ${token}`;
+    const headers = req.headers.set(TOKEN_HEADER_KEY, authToken);
+    authReq = req.clone({ headers });
+
+    return next.handle(authReq);
+  }
+}
+
+export const AUTH_INTERCEPTOR_PROVIDERS = [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }];
