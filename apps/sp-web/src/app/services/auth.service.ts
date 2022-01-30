d9fef7d46fe382ba3b2e214edfb73b0a4c49e147
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-01-30 00:16:03 -0300

    ✨ feat(web): auth service sign in and sign up routes

--- apps/sp-web/src/app/services/auth.service.ts
+++ apps/sp-web/src/app/services/auth.service.ts
@@ -1,12 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { environment } from 'apps/sp-web/src/environments/environment';
+import { Observable } from 'rxjs';
+
+const HTTP_OPTIONS = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  user = {
-    name: 'John Doe',
-    email: 'johndoe@teste.com',
-    userID: 1,
-  };
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    const url = environment.apiUrl + '/auth/sign-in';
+    return this.http.post(
+      url,
+      {
+        username,
+        password,
+      },
+      HTTP_OPTIONS
+    );
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    const url = environment.apiUrl + '/auth/sign-up';
+
+    return this.http.post(
+      url,
+      {
+        username,
+        email,
+        password,
+      },
+      HTTP_OPTIONS
+    );
+  }
 }
