f096d9c610f3424a1c8c1f07fdfab3a22c7a02bf
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-01-30 11:01:39 -0300

    📦 refactor(api): make service work with ministries mock instead of list items

--- apps/sp-api/src/app/services/ministry.service.ts
+++ apps/sp-api/src/app/services/ministry.service.ts
@@ -1,35 +1,48 @@
 import { Injectable } from '@nestjs/common';
 
+import { Ministry } from '@sp/api/models';
 import { MinistryListItemResponse, MinistryRequest } from '@sp/shared-interfaces';
 
 import { MINISTRIES_MOCK } from '../mocks';
 
 @Injectable()
 export class MinistryService {
-  private ministries = MINISTRIES_MOCK;
+  private ministries: Ministry[] = MINISTRIES_MOCK;
 
-  getMinistriesListItems(ministryID: string): MinistryListItemResponse[] {
-    return [];
-    // if (ministryID) {
-    //   const ministries = this.ministriesListItems.filter((ministry) => ministry.ministryID === Number(ministryID));
-    //   return ministries;
-    // }
-    // return this.ministriesListItems;
+  getMinistriesListItems(ministryID?: string): MinistryListItemResponse[] {
+    const ministryPredicate = (ministry: Ministry) => ministry.ministryID === Number(ministryID);
+
+    const filteredMinistries: Ministry[] = ministryID ? this.ministries.filter(ministryPredicate) : this.ministries;
+
+    const ministriesListItems: MinistryListItemResponse[] = filteredMinistries.map((ministry) => {
+      const ministryListItem: MinistryListItemResponse = {
+        ministryID: ministry.ministryID,
+        name: ministry.name,
+      };
+
+      return ministryListItem;
+    });
+
+    return ministriesListItems;
   }
 
   createMinistry(ministryListItem: MinistryRequest): MinistryListItemResponse {
-    return {
-      ministryID: 1,
-      name: 'Teste',
+    const ministry: Ministry = {
+      ministryID: this.ministries.length + 1,
+      name: ministryListItem.name,
+      ownerId: ministryListItem.ownerID,
+      members: [],
+      roles: [],
+      scales: [],
+      songs: [],
     };
 
-    // const ministryListItemResponse: MinistryListItemResponse = {
-    //   ministryID: this.ministriesListItems.length + 1,
-    //   name: ministryListItem.name,
-    // };
+    const ministryListItemResponse: MinistryListItemResponse = {
+      ministryID: ministry.ministryID,
+      name: ministry.name,
+    };
 
-    // this.ministriesListItems.push(ministryListItemResponse);
-
-    // return ministryListItemResponse;
+    this.ministries.push(ministry);
+    return ministryListItemResponse;
   }
 }
