edae6fb31d51ab71e0a980f071fef58931e45fa9
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-01-30 10:59:44 -0300

    📦 refactor: move token and local storage service to shared folder

--- apps/sp-web/src/app/services/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '../domain/auth/auth.service';
-export * from './local-storage.service';
-export * from './ministry.service';
-export * from './token.service';
---
+++ apps/sp-web/src/app/shared/services/index.ts
@@ -0,0 +1,4 @@
+export * from '../../domain/auth/auth.service';
+export * from './local-storage.service';
+export * from '../../domain/ministry/ministry.service';
+export * from './token.service';
