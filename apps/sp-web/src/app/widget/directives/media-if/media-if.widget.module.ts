b74924aa30755cd80151da49b82fb7a4af3ecbf7
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-02-02 11:34:50 -0300

    ✨ feat: media if directive

---
+++ apps/sp-web/src/app/widget/directives/media-if/media-if.widget.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { MediaIfDirective } from './media-if.directive';
+
+@NgModule({
+  declarations: [MediaIfDirective],
+  exports: [MediaIfDirective],
+})
+export class MediaIfDirectiveWidgetModule {}
