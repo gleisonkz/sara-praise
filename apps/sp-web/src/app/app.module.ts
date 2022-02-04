e87d790f69dec6587bcc722a2b0c3fb69f013480
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-02-04 02:27:06 -0300

    🏗 build: add ngneat hot toast

--- apps/sp-web/src/app/app.module.ts
+++ apps/sp-web/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HotToastModule } from '@ngneat/hot-toast';
 import { LogInterceptor } from 'apps/sp-web/src/app/shared/interceptors/log.interceptor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule, HttpClientModule, BrowserAnimationsModule, ReactiveFormsModule],
+  imports: [
+    HotToastModule.forRoot(),
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+  ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: LogInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
--- apps/sp-web/src/app/shared/interceptors/log.interceptor.ts
+++ apps/sp-web/src/app/shared/interceptors/log.interceptor.ts
@@ -5,11 +5,12 @@ import { Injectable } from '@angular/core';
 
 import { HttpMethod, LogService } from '@sp/web/shared/services';
 
-import { Observable, tap } from 'rxjs';
+import { HotToastService } from '@ngneat/hot-toast';
+import { catchError, EMPTY, Observable, tap } from 'rxjs';
 
 @Injectable()
 export class LogInterceptor implements HttpInterceptor {
-  constructor(private readonly logService: LogService) {}
+  constructor(private readonly logService: LogService, private readonly toastService: HotToastService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const path = request.url;
@@ -23,6 +24,10 @@ export class LogInterceptor implements HttpInterceptor {
           const responseData = event.body;
           this.logService.logHttp(path, payload, responseData, method, headers);
         }
+      }),
+      catchError(() => {
+        this.toastService.error('Ocorreu um erro ao processar a requisição.');
+        return EMPTY;
       })
     );
   }
