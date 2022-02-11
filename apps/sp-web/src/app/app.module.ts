d1efab92d3cda036051c019bc52507c8e9abad70
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-02-11 20:39:14 -0300

    📦 refactor: set global locale to pt-br

--- apps/sp-web/src/app/app.module.ts
+++ apps/sp-web/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { registerLocaleData } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import localePT from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +15,8 @@ import {
 import { LogInterceptor } from 'apps/sp-web/src/app/shared/interceptors/log.interceptor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+registerLocaleData(localePT);
 
 @NgModule({
   declarations: [AppComponent],
@@ -28,6 +32,7 @@ import { AppComponent } from './app.component';
     { provide: HTTP_INTERCEPTORS, useClass: LogInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
     { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
   ],
   bootstrap: [AppComponent],
 })
